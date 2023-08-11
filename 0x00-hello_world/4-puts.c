ba7a3cede1eaf364aa83b110c953b47720295d02
Alaa Fayed <alaafayed810@gmail.com>
2023-08-11 07:44:08 -0700

    description

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+1	#include <stdio.h>
+2
+3	/**
+	 * main - Entry point
+	 *
+	 * Description: A C program that prints with put function
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int main(void)
+{
+		puts("Programming is like building a multilingual puzzle");
+		return (0);
+}
