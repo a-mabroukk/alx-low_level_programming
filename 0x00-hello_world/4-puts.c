32cec21f47c35ee72cc2c9e628ecc3ac511c2dc5
Alaa Fayed <alaafayed810@gmail.com>
2023-08-11 08:29:57 -0700

    First C Project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
-1	#include <stdio.h>
-2
-3	/**
-	 * main - Entry point
-	 *
-	 * Description: A C program that prints with put function
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(void)
+#incude < stdio.h >
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-		puts("Programming is like building a multilingual puzzle");
-		return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
+
