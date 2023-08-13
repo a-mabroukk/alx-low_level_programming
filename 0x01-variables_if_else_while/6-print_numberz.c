78de73339200296f59bc79e52448e6ff4344790e
Alaa Fayed <alaafayed810@gmail.com>
2023-08-13 12:23:07 -0700

    Alaa

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
