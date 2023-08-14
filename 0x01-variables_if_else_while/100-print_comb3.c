db5827f8798da9d82b8121003e223e173a751888
Alaa Fayed <alaafayed810@gmail.com>
2023-08-14 11:14:40 -0700

    numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,9 +14,9 @@ int main(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		putchar(num1 + '0');
 		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
+			putchar(num1 + '0');
 			putchar(num2 + '0');
 
 			if (num1 < 8 || num2 < 9)
