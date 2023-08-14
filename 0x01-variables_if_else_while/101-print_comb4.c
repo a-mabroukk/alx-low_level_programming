e73e03fa19e0f1de7d99de23a9a0267acccaa7e4
Alaa Fayed <alaafayed810@gmail.com>
2023-08-14 12:23:28 -0700

    third num

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 				putchar(num2 + '0');
 				putchar(num3 + '0');
 
-				if (num1 < 7 || num2 < 8 || num3)
+				if (num1 < 7 || num2 < 8 || num3 < 9)
 				{
 					putchar(',');
 					putchar(' ');
