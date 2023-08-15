8f939365ee20bae2cc64ca8a63a7ca0d8ae8131e
Alaa Fayed <alaafayed810@gmail.com>
2023-08-15 15:09:19 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet
+ *
+ * Description: function that prints 10 times the alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	int c;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+}
