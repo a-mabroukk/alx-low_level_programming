a39df513456d6cf6c949e01d30e28303d66a2b30
Alaa Fayed <alaafayed810@gmail.com>
2023-08-16 16:46:41 -0700

    num

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,21 +27,17 @@ void print_times_table(int n)
 				prod = num * mult;
 
 				if (prod <= 9)
-				{
 					_putchar(' ');
-				}
 				if (prod <= 99)
-				{
 					putchar(' ');
-				}
 				if (prod >= 100)
-				{
 					_putchar((prod / 100) + '0');
 					_putchar((prod / 10) % 10 + '0');
-				}
 				else if (prod <= 99 && prod >= 10)
+				{
 					_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
+				}
 
 			}
 			_putchar('\n');
