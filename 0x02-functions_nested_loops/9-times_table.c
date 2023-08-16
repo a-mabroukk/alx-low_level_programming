d8ad0289c4ea2733332331411e6571c6c5853865
Alaa Fayed <alaafayed810@gmail.com>
2023-08-16 07:20:17 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,21 @@
 
 void times_table(void)
 {
-	int num1, num2;
+	int num1, num2' prod;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 <=9; num1++)
 	{
-		for (num2 = 0; num2 <= 10; num2++)
+		for (num2 = num1 + 1; num2 <= 10; num2++)
 		{
 			_putchar(num1 + '0');
-			_putchar('*');
-			_putchar((num2 / 10) + '0');
-			_putchar((num2 % 10) + '0');
-			_putchar('\n');
+			_putchar(num2 + '0');
+			_putchar(', ');
+			_putchar(' ');
+			if (prod <= 9)
+				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0';
 		}
+		_putchar('\n');
 	}
 }
