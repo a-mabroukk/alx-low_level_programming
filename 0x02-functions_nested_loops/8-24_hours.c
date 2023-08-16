203040210b6d54b25b5a0ac88da88428e187a1cd
Alaa Fayed <alaafayed810@gmail.com>
2023-08-16 05:18:51 -0700

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer
+ *
+ * Despription: function that prints every minute of the day of Jack Bauer,
+ *  starting from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int min = 00 : 00;
+
+	for (min = 00 : 00; min <= 23 : 59; min++)
+	{
+		while (min >= 00 : 00 && min <= 23 : 59)
+		{
+			_putchar(min);
+		}
+	}
+}
