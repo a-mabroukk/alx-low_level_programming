075d31b1037da98ebf9a1954f9345c3ed3bbdebd
Alaa Fayed <alaafayed810@gmail.com>
2023-08-16 07:43:08 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		for (mult = num + 1; mult <= 10; mult++)
+		for (mult = 1; mult <= 10; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
