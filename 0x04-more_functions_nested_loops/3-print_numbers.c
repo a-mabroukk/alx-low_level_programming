cc37b419a92b753e20ab8918e595b69e8e6270b5
Alaa Fayed <alaafayed810@gmail.com>
2023-08-17 12:25:52 -0700

    print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,18 +4,15 @@
  * print_numbers - prints the numbers
  *
  * Description: Write a function that prints the numbers, from 0 to 9
- *
- * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
 	int num = 0;
 
-	while (num < 10)
+	while (num <= 9)
 	{
 		_putchar(num);
 	}
 	_putchar('\n');
-	return (0);
 }
