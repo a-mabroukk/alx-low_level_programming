146dc477e478882400df0db18b3ef5d7cafa3733
Alaa Fayed <alaafayed810@gmail.com>
2023-08-17 13:41:24 -0700

    print numbers2

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers
+ *
+ * Description: Write a function that prints the numbers, from 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		if (num != 2 && num != 4)
+			_putchar(num + '0');
+			num++;
+	}
+	_putchar('\n');
+}
