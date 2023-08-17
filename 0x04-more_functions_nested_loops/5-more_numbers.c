3897453bde5ca4fff5a74b899839c3cb2cb44052
Alaa Fayed <alaafayed810@gmail.com>
2023-08-17 15:22:18 -0700

    more_number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,12 +16,10 @@ void more_numbers(void)
 		while (num <= 14)
 		{
 			if (num > 9)
-			{
-				_putchar((num % 10) + '0');
-			}
-			_putchar((num / 10) + '0');
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+			num++;
 		}
 	}
 	_putchar('\n');
-	num++;
 }
