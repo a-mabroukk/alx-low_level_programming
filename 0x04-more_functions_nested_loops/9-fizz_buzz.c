4d98349f4999b7688359128fe40c4e579dafd8e4
Alaa Fayed <alaafayed810@gmail.com>
2023-08-17 20:03:24 -0700

    p

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && !(n % 5) == 0)
-			_putchar("Fizz");
+			printf("Fizz");
 		else if (n % 5 == 0 && !(n % 3) == 0)
 			printf("Buzz");
 		else if (n % 3 == 0 && n % 5 == 0)
