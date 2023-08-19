e027473a5dc7ab3799a2247576843bd222ebf403
Alaa Fayed <alaafayed810@gmail.com>
2023-08-18 20:00:15 -0700

    l

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -20,12 +20,7 @@ void print_triangle(int size)
 		for (hight = 1; hight <= size; hight++)
 		{
 			for (base = 1; base <= size; base++)
-			{
-				if ((hight + base) <= size)
-					_putchar(' ');
-				else
-					_putchar('#');
-			}
+			_putchar('#');
 			_putchar('\n');
 		}
 	}
