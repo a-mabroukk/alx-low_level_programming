a8c054c88294e83151c6ab655e67eacb19063c73
Alaa Fayed <alaafayed810@gmail.com>
2023-08-20 05:42:11 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,12 +12,12 @@
 
 long long MaxPrime(long long num);
 {
-	long long MaxPrime = "-1";
+	long long CurrMaxPrime = "-1";
 	long long num = 612852475143;
 
 	if (num % 2 == 0)
 	{
-		MaxPrime = 2;
+		CurMaxPrime = 2;
 		while (num % 2 == 0)
 		{
 			num = num / 2;
@@ -27,11 +27,24 @@ long long MaxPrime(long long num);
 	{
 		while (num % j == 0)
 		{
-			MaxPrime = j;
+			CurrMaxPrime = j;
 			num = num / j;
 		}
 	}
 	if (num > 2)
-		MaxPrime = num;
-	return (MaxPrime);
+		CurrMaxPrime = num;
+	return (CurrMaxPrime);
+}
+
+
+
+
+int main(void);
+{
+	long long num;
+
+	num = 612852475143;
+
+	printf("Largest prime factor of %lld is: %lld\n", num, MaxPrime(num));
+	return (0);
 }
