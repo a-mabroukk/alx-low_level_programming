9c15a08dcba915a2b5ab0e666859560384d2ce84
Alaa Fayed <alaafayed810@gmail.com>
2023-08-20 14:20:06 -0700

    loll

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,11 +15,7 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n == 0)
-	{
-		_putcha('0');
-	}
-	if (n / 10)
+	if ((n / 10) > 0)
 	{
 		print_number(n / 10);
 		_putchar((n % 10) + '0');
