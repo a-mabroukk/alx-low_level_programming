dd5cf02b0de693b20fccc2461292b5be83f7ce5b
Alaa Fayed <alaafayed810@gmail.com>
2023-08-22 13:44:37 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be print
+ *
+ * Description: Write a function that prints a string
+ */
+
+void _puts(char *str)
+{
+	puts(*str);
+}
