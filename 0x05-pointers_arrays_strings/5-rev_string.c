0f16593f4d8bbe0271da3164f3b8be0accc1c835
Alaa Fayed <alaafayed810@gmail.com>
2023-08-22 19:14:42 -0700

    l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to be reverse
+ *
+ * Description: Write a function that reverses a string
+ */
+
+void rev_string(char *s)
+{
+	int L, i;
+
+	for (L = 0; *s != '\0'; s++)
+	{
+		++L;
+	}
+	if (i < L)
+	{
+		temp = s[i];
+		s[i] = s[L - 1];
+		s[L - 1] = temp;
+		i++;
+	}
+}
