2d73dec6eecac40eb504445e6634094c87ab8d5c
Alaa Fayed <alaafayed810@gmail.com>
2023-08-23 17:51:32 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string
+ *
+ * @dest: a string in which to store the value
+ *
+ * @src: a string to be copy
+ *
+ * Description: Write a function that copies the string
+ *
+ * Return: value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	_strcpy(dest, src);
+	return (dest);
+}
