59ee75953b116384c842739ab90b4a534e5e19a0
Alaa Fayed <alaafayed810@gmail.com>
2023-08-24 17:43:44 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string to be compare
+ *
+ * @s2: second string to be compare
+ *
+ * Description: Write a function that compares two strings
+ *
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0')
+	{
+		while (*s2 != '\0')
+		{
+			if (*s1 == *s2)
+			{
+				return (0);
+			}
+			s2++;
+		}
+		s1++;
+	}
+	return (*s1 - *s2);
+}
