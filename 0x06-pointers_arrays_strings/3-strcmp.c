4084d9d65ce71c6ef7f95a319e13ad99a5d2a980
Alaa Fayed <alaafayed810@gmail.com>
2023-08-24 18:21:10 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,17 +14,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		while (*s2 != '\0')
-		{
-			if (*s1 == *s2)
-			{
-				return (0);
-			}
+		s1++;
 		s2++;
-		}
-	s1++;
 	}
 	return (*s1 - *s2);
 }
