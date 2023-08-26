61c07dd29dca59948eecef954e7a4c111ffe6708
Alaa Fayed <alaafayed810@gmail.com>
2023-08-25 19:11:25 -0700

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13
+ *
+ * @str: a string to be encode
+ *
+ * Description: Write a function that encodes a string using rot13
+ *
+ * Return: value
+ */
+
+char *rot13(char *str)
+{
+	char abc[] = " ";
+	char ABC[] = " ";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; abc[j] != '\0'; j++)
+		{
+			if (str[i] == abc[j])
+			{
+				str[i] = ABC[j];
+			}
+		}
+	}
+	return (str);
+}
