bf38ef04da30eebaf1f123bd230a603a2e565d1c
Alaa Fayed <alaafayed810@gmail.com>
2023-08-26 10:57:02 -0700

    rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,27 +12,27 @@
 
 char *rot13(char *str)
 {
-	int abc[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	char abc[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
 		'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
 		'W', 'X', 'Y', 'Z'};
-	int ABC[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	char ABC[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 		'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
 		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 		'J', 'K', 'L', 'M'};
-	int i, j = 0;
+	int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (abc[j] != '\0' && str[i] != abc[j])
+		for (j = 0; abc[j] != '\0'; j++)
 		{
 			if (str[i] == abc[j])
 			{
 				str[i] = ABC[j];
+				break;
 			}
-			j++;
 		}
 	}
 	return (str);
