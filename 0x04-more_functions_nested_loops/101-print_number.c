bb6ec76da5049ec851784dceba6152ffba64333c
Alaa Fayed <alaafayed810@gmail.com>
2023-08-27 07:06:19 -0700

    l

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,12 +10,30 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int tens, digit, positive = n;
+	doule t_beg = 1;
+
+	if (n == 0)
 	{
-		_putchar('-');
-		n = n * (-1);
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_purchar('-');
+		}
+		while (t_beg <= positive)
+		{
+			t_beg *= 10;
+		tens = t_beg / 10;
+
+		while (tens >= 10)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
 }
