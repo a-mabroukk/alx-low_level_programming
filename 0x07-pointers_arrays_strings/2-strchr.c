3341f5a96374708bea89516400a1e941b0bfc57d
Alaa Fayed <alaafayed810@gmail.com>
2023-08-28 13:13:22 -0700

    locate

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,14 +14,9 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (c == s[i])
 	{
-		if (c == s[i])
-		{
-			return (s[i]);
-		}
+		return (s);
 	}
 	return ('\0');
 }
