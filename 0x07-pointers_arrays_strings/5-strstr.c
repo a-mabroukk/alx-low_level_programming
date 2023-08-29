ba63b7c10919cdd8d278ae784c6a38e49f3785f5
Alaa Fayed <alaafayed810@gmail.com>
2023-08-29 11:20:38 -0700

    add

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,15 +18,11 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[j])
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			for (j = 0; needle[j] != '\0'; j++)
+			if (haystack[i] == needle[j])
 			{
-				if (haystack[i] == needle[j])
-				{
-					c++;
-					return (&haystack[j]);
-				}
+				c++;
 			}
 		}
 		if (needle[j] != haystack[i])
