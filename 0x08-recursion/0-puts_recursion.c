c91a1ea8ed80f57543379849dbf8e1d130005b39
Alaa Fayed <alaafayed810@gmail.com>
2023-08-31 10:57:19 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ *
+ * @s: a string to be print
+ *
+ * Description: Write a function that prints a string, followed by a new line
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+}
