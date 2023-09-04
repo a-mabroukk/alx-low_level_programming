97667622b02225a2c6fc2598ef3f15e85f96ed3f
Alaa Fayed <alaafayed810@gmail.com>
2023-09-04 13:44:20 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: the number of arguments passed to the program
+ *
+ * @arv: a one-dimensional array of stringsa
+ *
+ * Description: Write a program that prints its name, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+		return (0);
+}
