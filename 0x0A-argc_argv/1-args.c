d4d6aad387090e83918000720e7b9d9fddd8c82e
Alaa Fayed <alaafayed810@gmail.com>
2023-09-04 15:58:42 -0700

    num

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the number of arguments passed into it
+ *
+ * @argc: number of arguments passed to the program
+ *
+ * @argv: one-dimensional array of strings
+ *
+ * Description: Write a program that prints the number of arguments passed into
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc - 1; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
