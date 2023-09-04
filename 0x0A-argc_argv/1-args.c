ed442fec03641f43fc61cdaf81fc7e8120a5a4c3
Alaa Fayed <alaafayed810@gmail.com>
2023-09-04 16:48:27 -0700

    num

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,13 +12,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	int i;
-
-	for (i = 1; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
