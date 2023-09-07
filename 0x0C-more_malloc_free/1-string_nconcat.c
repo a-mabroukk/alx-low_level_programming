ea0dc9c1322444d78329429fc184b687bcbf80fd
Alaa Fayed <alaafayed810@gmail.com>
2023-09-07 15:56:59 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,8 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (c1 = 0; c1 < size1; c1++)
 	{
-		if (c1 < size1)
-			str[c1] = s1[c1];
+		str[c1] = s1[c1];
 	}
 	for (c2 = 0; c2 < n; c2++)
 	{
