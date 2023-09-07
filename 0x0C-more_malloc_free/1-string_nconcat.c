55e8308630fa7272e1f741340229a36ee9b56bb5
Alaa Fayed <alaafayed810@gmail.com>
2023-09-07 16:42:09 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,6 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > size2)
 	{
 		n = size2;
+		str = malloc(sizeof(char) * (size1 + size2 + 1));
 	}
 	str = malloc(sizeof(char) * (size1 + n + 1));
 	if (str == NULL)
