1a73e44d5ba4b448f75c4988d4eca6b8a5b433bb
Alaa Fayed <alaafayed810@gmail.com>
2023-09-08 15:24:11 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,11 +40,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		mem = malloc(new_size);
 	}
-	if (mem == NULL)
-	{
-		free(ptr);
-		return (mem);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
