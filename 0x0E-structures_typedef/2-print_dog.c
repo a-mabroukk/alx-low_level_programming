466a87d7c99d1ab679a8f9eaa00e17fd94cf997a
Alaa Fayed <alaafayed810@gmail.com>
2023-09-12 19:41:57 +0200

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*print_dog - prints a struct dog
+*@d: a variable of type struct dog should be initialized
+*Description: Write a function that prints a struct dog
+*/
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+d->age ? (printf("Age: %f\n", d->age)) : (printf("Age: (nile)\n"));
+d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nile)\n"));
+}
+}
