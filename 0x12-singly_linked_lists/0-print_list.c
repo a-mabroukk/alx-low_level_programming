cd454f977e30177ea8569a8177b0675c17151de5
Alaa Fayed <alaafayed810@gmail.com>
2023-09-22 00:00:59 +0200

    list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+*print_list - prints all the elements of a list_t list
+*@h: a pointer to list
+*Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+const list_t *ptr = NULL;
+size_t counter = 0;
+if (h == NULL)
+{
+printf("[0] (nil)\n");
+}
+ptr = h;
+while (ptr != NULL)
+{
+printf("[%d] %s\n", ptr->len, ptr->str);
+counter++;
+ptr = ptr->next;
+}
+return (counter);
+}
