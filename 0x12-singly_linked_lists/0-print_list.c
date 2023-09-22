dea6d865801d5680b54e2c93f2ebdf6c155ec05b
Alaa Fayed <alaafayed810@gmail.com>
2023-09-22 02:09:54 +0200

    list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,19 @@
 size_t print_list(const list_t *h)
 {
 size_t counter = 0;
-const list_t *ptr = NULL;
-ptr = h;
 
-while (ptr != NULL)
+while (h != NULL)
 {
-if (ptr->str != NULL)
+if (h->str != NULL)
 {
-printf("[%d] %s\n", ptr->len, ptr->str);
+printf("[%d] %s\n", h->len, h->str);
 }
-else if (ptr->str == NULL)
+else if (h->str == NULL)
 {
 printf("[0] (nil)\n");
 }
 counter++;
-ptr = ptr->next;
+h = h->next;
 }
 return (counter);
 }
