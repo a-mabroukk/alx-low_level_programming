bba6a84e7ed2f14c6e6fdbce9023de8dea121844
Alaa Fayed <alaafayed810@gmail.com>
2023-09-22 06:02:21 +0200

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,11 +7,13 @@
 void free_list(list_t *head)
 {
 list_t *temp;
+temp = head;
 while (head != NULL)
 {
 temp = head;
-}
 head = head->next;
-free(head->str);
+free(temp->str);
 free(temp);
+}
+free(head);
 }
