8523b198a7fb609d446f5b1df496e3866d5e5c9b
Alaa Fayed <alaafayed810@gmail.com>
2023-09-23 17:47:24 +0200

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,22 +1,23 @@
 #include "3-calc.h"
 /**
-*get_op_func - selects the correct function to perform the operation 
+*get_op_func - selects the correct function to perform the operation
 *asked by the user
 *@s: is the operator passed as argument to the program
-*Description: Write a function thatselects the correct function to perform the operation
+*Description: Write a function thatselects the correct function to
+*perform the operation
 *asked by the user
 *Return: NULL
 */
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 
 int i = 0;
 while (ops[i].op != NULL)
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,22 +10,22 @@ int main(int argc, char *argv[])
 {
 char *s = argv[2];
 int a, b;
-if (argc != 4) 
+if (argc != 4)
 {
 printf("%s\n", "Error");
-exit (98);
+exit(98);
 }
 if ((get_op_func(argv[2]) == NULL) || (s[1] != '\0'))
 {
 printf("%s\n", "Error");
-exit (99);
+exit(99);
 }
 a = atoi(argv[1]);
 b = atoi(argv[3]);
 if (b == 0 && (s[0] == '/' || s[0] == '%'))
 {
 printf("%s\n", "Error");
-exit (100);
+exit(100);
 }
 a = atoi(argv[1]);
 b = atoi(argv[3]);
