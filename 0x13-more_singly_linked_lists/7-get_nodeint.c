a1f89edb5cb70deccefa37bba90763ae9789e64b
Alaa Fayed <alaafayed810@gmail.com>
2023-09-26 00:36:00 +0200

    8

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 listint_t *node;
 unsigned int c;
-for (c = 0; c < index; c++)
-{
 if (head == NULL)
 {
 return (NULL);
 }
+for (c = 0; c < index; c++)
+{
 node = head;
 if (node == NULL)
 {
