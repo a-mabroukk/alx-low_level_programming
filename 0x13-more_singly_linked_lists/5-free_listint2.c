f911aab0f6f889eaf6d84eaa4cb6c86e281367d7
Alaa Fayed <alaafayed810@gmail.com>
2023-09-25 19:46:06 +0200

    6

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+*free_listint2 - frees a listint_t list
+*
+*@head: a pointer to the head of the list
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *node, *temp;
+
+temp = *head;
+while (temp != NULL)
+{
+node = temp->next;
+free(temp);
+temp = node;
+}
+*head = NULL;
+}
