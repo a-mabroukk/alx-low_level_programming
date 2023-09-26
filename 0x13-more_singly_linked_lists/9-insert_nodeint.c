42bb8104db2405b892731f5cc06d0a696779a530
Alaa Fayed <alaafayed810@gmail.com>
2023-09-26 23:08:01 +0200

    10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *ptr = *head;
-listint_t *ptr2;
+listint_t *ptr2 = *head;
 unsigned int position;
 
 ptr2 = malloc(sizeof(listint_t));
