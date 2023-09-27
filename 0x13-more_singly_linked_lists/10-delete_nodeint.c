468178246252a02da16d4d54266e8743af859455
Alaa Fayed <alaafayed810@gmail.com>
2023-09-27 05:41:20 +0200

    11

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,14 +14,17 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *prev = *head;
 listint_t *curr = *head;
 unsigned int position;
-
+if (*head == NULL)
+{
+return (-1);
+}
 if (curr == NULL)
 {
 return (-1);
 }
 if (index == 0)
 {
-*head = curr->next;
+*head = (*head)->next;
 free(curr);
 return (1);
 }
