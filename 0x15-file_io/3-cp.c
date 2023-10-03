824688865d183200d819d1f416a796bbf324dcb1
Alaa Fayed <alaafayed810@gmail.com>
2023-10-03 16:47:20 +0200

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,12 +36,10 @@ b1 = read(file_from, buf, 1024);
 while (b1 == 1024)
 {
 if (b1 == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 b2 = write(file_to, buf, b1);
 if (b2 < b1)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 if (close(file_from) == -1)
 {
@@ -50,6 +48,5 @@ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 if (close(file_to) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-}
-return (0);
+}return (0);
 }
