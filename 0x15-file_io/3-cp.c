5a12b98318b0adfb1247ca0b0dc24dcb6bf51d69
Alaa Fayed <alaafayed810@gmail.com>
2023-10-03 05:56:24 +0200

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,16 +18,16 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 file_from = open(argv[1], O_RDONLY);
-if (file_from == 1)
+if (file_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (file_to == 1)
+if (file_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write file_to %s\n", argv[2]);
-exit(99);
+close(file_from), exit(99);
 }
 while (b1 == 1024)
 {
@@ -45,6 +45,8 @@ if (close(file_from) == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 }
 if (close(file_to) == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+}
 return (0);
 }
