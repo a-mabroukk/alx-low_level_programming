b6e35e651c16ecb7b51684cf0b78bdeffcbb69bb
Alaa Fayed <alaafayed810@gmail.com>
2023-10-03 05:49:13 +0200

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,10 +15,8 @@ int file_from, file_to;
 int b1 = 1024, b2 = 0;
 char buf[1024];
 if (argc != 3)
-{
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
-}
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
@@ -35,26 +33,18 @@ while (b1 == 1024)
 {
 b1 = read(file_from, buf, 1024);
 if (b1 == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file file_from %s\n", argv[1]);
-exit(98);
-}
+dprintf(STDERR_FILENO, "Error: Can't read from file file_from %s\n"
+, argv[1]), exit(98);
 b2 = write(file_to, buf, b1);
 if (b2 < b1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file file_to %s\n", argv[2]);
-exit(99);
-}
+dprintf(STDERR_FILENO, "Error: Can't read from file file_to %s\n"
+, argv[2]), exit(99);
 }
 if (close(file_from) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_from);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 }
 if (close(file_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_to);
-exit(100);
-}
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 return (0);
 }
